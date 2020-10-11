8bc62e66e99f6d7e517d8f488436d6fd049582f8
Kyle Janssen <ga2947@wayne.edu>
2020-10-11 17:46:38 -0400

    fixed the error when logging in

--- src/main/resources/static/webpageFun.js
+++ src/main/resources/static/webpageFun.js
@@ -18,13 +18,48 @@ function loginCheck() {
       let isError = params.get("error");
       let isLoggedOut = params.get("logout");
 
-      if(isError != null){
+      if (isError != null){
           // change this to be more user friendly upon error
-          alert("deny")
+          //alert("deny")
+          //hide main content on page and show login info
+          document.getElementById("mainContainer").style.display = "none";
+          document.getElementById("loginContainer").style.display = "block";
+          //hide/show login confirmation or error
           document.getElementById("confirm").style.display = "none";
           document.getElementById("deny").style.display = "block";
+          //logout button hidden
+          document.getElementById("logoutButton").style.display = "none";
+
+      } else {
+        //show login success
+        document.getElementById("deny").style.display = "none";
+        document.getElementById("confirm").style.display = "block";
+        //show logout button and hide login -- doesnt work right now
+        //document.getElementById("loginButton").style.display = "none";
+        //document.getElementById("logoutButton").style.display = "block";
       }
-      else {
+
+      if(isLoggedOut != null){
+          alert("You have been logged out.");
+          document.getElementById("logoutButton").style.display = "none";
+          document.getElementById("loginButton").style.display = "block";
+      }
+}
+/*
+function loginCheck() {
+      const getParams = window.location.search;
+      const params = new URLSearchParams(getParams);
+
+      let isError = params.get("error");
+      let isLoggedOut = params.get("logout");
+
+      if (isError != null){
+          // change this to be more user friendly upon error
+          //alert("deny")
+          document.getElementById("confirm").style.display = "none";
+          document.getElementById("deny").style.display = "block";
+          document.getElementById("logoutButton").style.display = "none";
+      } else {
         document.getElementById("deny").style.display = "none";
         document.getElementById("confirm").style.display = "block";
         document.getElementById("loginButton").style.display = "none";
@@ -37,3 +72,4 @@ function loginCheck() {
           document.getElementById("loginButton").style.display = "block";
       }
 }
+*/
