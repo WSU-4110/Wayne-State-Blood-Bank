f0e917da288e3c957c44524ff09d2abbcde69e1c
Jacob Boesch <jacobtboesch@gmail.com>
2020-10-23 19:06:16 -0400

    Created ProfileControllerService

---
+++ src/main/java/org/redcrosswarriors/controllerservice/ProfileControllerService.java
@@ -0,0 +1,65 @@
+package org.redcrosswarriors.controllerservice;
+
+import org.redcrosswarriors.model.Profile;
+import org.redcrosswarriors.model.input.EditProfileInput;
+import org.redcrosswarriors.repository.UserDetailsRepository;
+import org.redcrosswarriors.security.AccountDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Map;
+import java.util.HashMap;
+
+@Service
+public class ProfileControllerService {
+
+    @Autowired
+    private UserDetailsRepository repository;
+
+    @Autowired
+    private AccountDetailsService accountDetailsService;
+
+    @Transactional
+    public ResponseEntity<Object> updateProfile(String email, EditProfileInput input){
+        Map<String, Object> responseObject = new HashMap<String, Object>();
+
+        try{
+            if(input.getPassword() != null){
+                accountDetailsService.updatePassword(email, input.getPassword());
+            }
+
+            repository.updateProfileByEmail(
+                    email,
+                    input.getFirstName(),
+                    input.getLastName(),
+                    input.getPhoneNumber(),
+                    input.getBloodDonorStatus());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            responseObject.put("message", "An unknown error has occurred");
+            return new ResponseEntity<Object>(responseObject, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        responseObject.put("message", "Successfully updated profile");
+        return new ResponseEntity<Object>(responseObject, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Object> getProfile(String email){
+        Profile profile;
+        try{
+            profile = repository.getProfileByEmail(email);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            Map<String, String> errorResponse = new HashMap<String,String>();
+            errorResponse.put("message", "An unknown error has occurred");
+            return new ResponseEntity<Object>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<Object>(profile, HttpStatus.OK);
+    }
+}
---
+++ src/test/java/org/redcrosswarriors/controllerservice/TestProfileControllerService.java
@@ -0,0 +1,62 @@
+package org.redcrosswarriors.controllerservice;
+
+import org.junit.jupiter.api.Test;
+import org.redcrosswarriors.model.Profile;
+import org.redcrosswarriors.model.input.EditProfileInput;
+import org.redcrosswarriors.repository.UserDetailsRepository;
+import org.redcrosswarriors.security.AccountDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class TestProfileControllerService {
+
+    @MockBean
+    private AccountDetailsService accountService;
+
+    @MockBean
+    private UserDetailsRepository repository;
+
+    @Autowired
+    private ProfileControllerService service;
+
+    @Test
+    public void testUpdateProfile(){
+        EditProfileInput input = new EditProfileInput();
+        input.setFirstName("John");
+        input.setLastName("Doe");
+        input.setPassword("Password1234");
+        input.setBloodDonorStatus("Y");
+        input.setPhoneNumber("1111111111");
+
+        ResponseEntity<Object> response = service.updateProfile("user@wayne.edu", input);
+        verify(accountService).updatePassword("user@wayne.edu", "Password1234");
+        verify(repository).updateProfileByEmail("user@wayne.edu",
+                input.getFirstName(), input.getLastName(), input.getPhoneNumber(), input.getBloodDonorStatus());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void testGetProfile(){
+        Profile profile = new Profile();
+        profile.setFirstName("John");
+        profile.setLastName("Doe");
+        profile.setBirthDay("0000-01-01");
+        profile.setBloodType("A+");
+        profile.setEmail("john@wayne.edu");
+        profile.setBloodDonorStatus("Y");
+        when(repository.getProfileByEmail("john@wayne.edu")).thenReturn(profile);
+        ResponseEntity<Object> response = service.getProfile("john@wayne.edu");
+        verify(repository).getProfileByEmail("john@wayne.edu");
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+}
