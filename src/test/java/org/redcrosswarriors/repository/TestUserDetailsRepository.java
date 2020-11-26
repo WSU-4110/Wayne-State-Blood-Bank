17dfe6d585e0012d8e11acd0916ac15be4370c57
Lloyd Brombach <lbrombach2@gmail.com>
2020-11-26 08:52:54 -0500

    adding back my unit tests for homework commit

---
+++ src/test/java/org/redcrosswarriors/repository/TestUserDetailsRepository.java
@@ -0,0 +1,50 @@
+package org.redcrosswarriors.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.redcrosswarriors.emailservice.SendMail;
+import org.redcrosswarriors.model.Profile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.transaction.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@Transactional
+public class TestUserDetailsRepository extends SendMail {
+
+    @Autowired
+    private UserDetailsRepository repository;
+
+    @Test
+    @Sql("/test_get_profile.sql")
+    public void testGetProfile(){
+        Profile profile = repository.getProfileByEmail("user@wayne.edu");
+        assertEquals("John", profile.getFirstName());
+    }
+
+    @Test
+    @Sql("/test_get_profile.sql")
+    public void testUpdateProfile(){
+        // update name to Jane
+        repository.updateProfileByEmail("user@wayne.edu", "Jane",
+                "Smith", "1111111112", "N");
+
+        Profile profile1 = repository.getProfileByEmail("user@wayne.edu");
+        assertEquals("Jane", profile1.getFirstName());
+    }
+
+    @Test
+    @Sql("/test_get_profile.sql")
+    void getAllProfiles() {
+        List<Profile> profiles = repository.getAllProfiles();
+        assertEquals(1, profiles.size());
+    }
+}
