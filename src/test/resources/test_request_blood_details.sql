c212049bad705ec782e4560b52827a4dfb79f0d4
tia-gijo <51178555+tiagijo@users.noreply.github.com>
2020-12-02 13:08:25 -0500

    jacob

---
+++ src/test/resources/test_request_blood_details.sql
@@ -0,0 +1,8 @@
+INSERT INTO accounts(email, password) VALUES('user1@wayne.edu', 'Password1234');
+INSERT INTO user_details
+VALUES(SELECT id FROM accounts WHERE email = 'user1@wayne.edu', 'John', 'Doe', '1900-01-01', 'Y', '1111111111', 'A+');
+
+
+INSERT INTO accounts(email, password) VALUES('user2@wayne.edu', 'Password1234');
+INSERT INTO user_details
+VALUES(SELECT id FROM accounts WHERE email = 'user2@wayne.edu', 'Johny', 'Doeui', '1901-01-01', 'Y', '1111111110', 'A+');
